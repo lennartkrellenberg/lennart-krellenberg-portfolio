2dd7a7315ea9f3df27f0ff2b76fb3895fd09bc45
Lennart Krellenberg <lennart.krellenberg2002@gmail.com>
2021-05-18 22:37:52 +0200

    added SCSS to Hero Section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as style from "../style/global.module.scss"
 import { Helmet } from "react-helmet"
-import Person from "../images/person.svg";
+import { StaticImage } from "gatsby-plugin-image"
 
 
 
@@ -23,8 +23,8 @@ const IndexPage = () => {
       <section className={style.heroSection}>
         <div className={style.heroContent}>
           <div className={style.heroLeft}>
-            <h3>Hi, I'm Lennart👋🏻</h3>
-            <h2>App and Front-End Developer</h2>
+            <h3>Hi, I'm Lennart <span role="img" aria-label="shake-hand">👋🏻</span></h3>
+            <h2><span className={style.blue}>App</span> and <span className={style.pink}>Front-End</span> Developer</h2>
             <div className={style.heroButtons}>
               <button className={style.btn1}>Projekte</button>
               <button className={style.btn2}>Kontakt</button>
@@ -33,7 +33,13 @@ const IndexPage = () => {
 
 
           <div className={style.heroRight}>
-            <Person /></div>
+            <StaticImage className={style.person} src="../images/person.svg"
+              alt="Person SVG"
+              placeholder="none"
+
+            />
+
+          </div>
 
         </div>
       </section>
